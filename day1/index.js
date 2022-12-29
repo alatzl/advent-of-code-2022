159b71d2ef0a95688f34dc940a42cbaa6241a842
Alexandra Atzl <alatzl@gmail.com>
2022-12-28 23:52:56 -0500

    Day 1 Code.

---
+++ day1/index.js
@@ -0,0 +1,52 @@
+const fs = require('fs/promises');
+const path = require('path');
+const _ = require('lodash');
+
+// Find elf with most snacks
+const getHungriestElf = async() => {
+  try {
+    const parsed = await getData();
+    const elfCalories =  _.map(parsed, (elfArr, idx) => {
+      return  _.sum(elfArr);
+    });
+    return _.max(elfCalories);
+  } catch (err) {
+    console.log(`OOPS! ${err}`)
+  }
+}
+
+const getData = async () => {
+  const data = await fs.readFile(__dirname + "/input.txt", "utf-8")
+
+    const elfArrays = data.toString().split(`\n\n`).map(item => {
+        return item.split(`\n`);
+      });
+
+    const parsed = elfArrays.map((arr) => {
+      return _.compact(arr.map((item) => {
+        return parseInt(item);
+      }));
+    });
+
+    return parsed;
+};
+
+getHungriestElf();
+
+const getTopThreeElves = async() => {
+  try {
+    const parsed = await getData();
+    const top3 = _(parsed)
+      .map((calArr) => _.sum(calArr))
+      .sort()
+      .reverse()
+      .slice(0, 3)
+      .sum();
+
+    console.log(`the top 3 summed: ${top3}`);
+  } catch (err) {
+    console.log(`OOPS! ${err}`)
+  }
+}
+
+getTopThreeElves();
